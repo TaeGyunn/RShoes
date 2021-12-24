2daf440fc0f3772eea934a0c1cc430cdb9fc85f6
TaeGyun <taekuen98@naver.com>
2021-12-24 20:49:25 +0900

    21.12.24 findbyname

--- src/main/java/resell/shoes/RShoes/repository/CategoryRepository.java
+++ src/main/java/resell/shoes/RShoes/repository/CategoryRepository.java
@@ -2,11 +2,14 @@ package resell.shoes.RShoes.repository;
 
 import org.springframework.stereotype.Repository;
 import resell.shoes.RShoes.dto.CategoryDTO;
+import resell.shoes.RShoes.entity.Category;
 
 @Repository
 public interface CategoryRepository {
 
     Boolean checkByName(String categoryName);
+
+    Category findByName(String categoryName);
 
     Integer insertCategory(CategoryDTO categoryDTO);
 
