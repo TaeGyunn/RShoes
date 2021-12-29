ae4e344403f3f705ee94d8e3fb587f96cb4d6665
TaeGyun <taekuen98@naver.com>
2021-12-29 19:23:14 +0900

    21.12.29 InsertBrand

--- src/main/java/resell/shoes/RShoes/controller/AdminController.java
+++ src/main/java/resell/shoes/RShoes/controller/AdminController.java
@@ -7,7 +7,10 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import resell.shoes.RShoes.dto.CategoryDTO;
-import resell.shoes.RShoes.service.AdminService;
+import resell.shoes.RShoes.dto.BrandDTO;
+import resell.shoes.RShoes.repository.BrandRepository;
+import resell.shoes.RShoes.service.BrandService;
+import resell.shoes.RShoes.service.CategoryService;
 import resell.shoes.RShoes.service.Helper;
 import resell.shoes.RShoes.util.Response;
 
@@ -17,8 +20,20 @@ import resell.shoes.RShoes.util.Response;
 @RequestMapping(value = "/admin")
 public class AdminController {
 
-    private final AdminService adminService;
     private final Response response;
+    private final CategoryService categoryService;
+    private final BrandService brandService;
+
+    @PostMapping(value = "/insertBrand")
+    public ResponseEntity<?> insertBrand(@Validated @RequestBody BrandDTO brand, Errors errors){
+
+        if(errors.hasErrors()){
+            return response.invalidFields(Helper.refineErrors(errors));
+        }
+
+        return brandService.insertBrand(brand);
+
+    }
 
     @PostMapping(value = "/insertCategory")
     public ResponseEntity<?> addCategory(@Validated @RequestBody CategoryDTO category, Errors errors){
@@ -27,7 +42,8 @@ public class AdminController {
             return response.invalidFields(Helper.refineErrors(errors));
         }
 
-        return adminService.insertCategory(category);
+        return categoryService.insertCategory(category);
+
     }
 
     @DeleteMapping(value = "/deleteCategory")
@@ -37,7 +53,7 @@ public class AdminController {
             return response.invalidFields(Helper.refineErrors(errors));
         }
 
-        return adminService.deleteCategory(category);
+        return categoryService.deleteCategory(category);
 
     }
 
