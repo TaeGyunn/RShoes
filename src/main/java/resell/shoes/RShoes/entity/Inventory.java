23c970ab25b07d7600335498423cffe9094dba41
TaeGyun <taekuen98@naver.com>
2021-12-29 19:25:06 +0900

    21.12.29 new entity

---
+++ src/main/java/resell/shoes/RShoes/entity/Inventory.java
@@ -0,0 +1,14 @@
+package resell.shoes.RShoes.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Inventory {
+
+    private Long inventoryNo;       //인벤토리no
+
+    private String inventoryStatus; //상태
+}
