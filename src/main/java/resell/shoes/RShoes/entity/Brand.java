366df4bc4c635199754101236b64d0c65ed55530
TaeGyun <taekuen98@naver.com>
2021-12-29 19:24:03 +0900

    21.12.29 entity modify

--- src/main/java/resell/shoes/RShoes/entity/Brand.java
+++ src/main/java/resell/shoes/RShoes/entity/Brand.java
@@ -1,19 +1,13 @@
 package resell.shoes.RShoes.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 public class Brand {
     
     private Long brandNo;       // 브랜드 번호
-    private User user;          // 유저
     private String brandName;    // 브랜드 이름
 
-    public Brand(User user, String brandName){
-        this.user = user;
-        this.brandName = brandName;
-    }
 }
