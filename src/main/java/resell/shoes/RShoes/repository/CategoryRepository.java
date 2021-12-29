26ed622e5a013a18309ff4186f10e6124c957f35
TaeGyun <taekuen98@naver.com>
2021-12-29 19:24:52 +0900

    21.12.29 category Create / Delete

--- src/main/java/resell/shoes/RShoes/repository/CategoryRepository.java
+++ src/main/java/resell/shoes/RShoes/repository/CategoryRepository.java
@@ -6,12 +6,16 @@ import resell.shoes.RShoes.entity.Category;
 
 @Repository
 public interface CategoryRepository {
-
+    
+    // 카테고리이름 확인
     Boolean checkByName(String categoryName);
-
+    
+    // 카테고리 아이디로 카테고리 찾기
     Category findByName(String categoryName);
-
+    
+    // 카테고리 생성
     Integer insertCategory(CategoryDTO categoryDTO);
-
+    
+    // 카테고리 제거
     Integer deleteCategory(CategoryDTO category);
 }
---
+++ src/main/java/resell/shoes/RShoes/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,56 @@
+package resell.shoes.RShoes.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import resell.shoes.RShoes.dto.CategoryDTO;
+import resell.shoes.RShoes.repository.CategoryRepository;
+import resell.shoes.RShoes.service.CategoryService;
+import resell.shoes.RShoes.util.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final Response response;
+
+    @Override
+    public ResponseEntity<?> insertCategory(CategoryDTO category) {
+
+        Map<String, Boolean> map = new HashMap<>();
+
+        if(categoryRepository.checkByName(category.getName())){
+            map.put("insert", false);
+            return response.fail(map, "카테고리가 이미 존재합니다", HttpStatus.BAD_REQUEST);
+        }
+
+        categoryRepository.insertCategory(category);
+        map.put("insert", true);
+
+        return response.success(map, "카테고리 생성 완료", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<?> deleteCategory(CategoryDTO category) {
+
+        Map<String, Boolean> map = new HashMap<>();
+
+        if(!categoryRepository.checkByName(category.getName())){
+            map.put("delete", false);
+            return response.fail(map, "카테고리가 없습니다", HttpStatus.BAD_REQUEST);
+        }
+
+        categoryRepository.deleteCategory(category);
+
+        map.put("delete", true);
+
+        return response.success(map, "카테고리 삭제 완료", HttpStatus.OK);
+    }
+}
