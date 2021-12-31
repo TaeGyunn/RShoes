81cce794d5067c6144c36e94e652d68e368a4075
TaeGyun <taekuen98@naver.com>
2021-12-31 16:41:40 +0900

    21.12.31 생성자

--- src/main/java/resell/shoes/RShoes/entity/Delivery.java
+++ src/main/java/resell/shoes/RShoes/entity/Delivery.java
@@ -8,8 +8,14 @@ import resell.shoes.RShoes.dto.Status;
 @RequiredArgsConstructor
 public class Delivery {
 
-private Long deliveryNo;         // 배달번호
+    private Long deliveryNo;    // 배달번호
     private String address;     // 주소
     private Integer zip_code;   // 우편번호
     private Status status;      // 배달상태
+
+    public Delivery(String address, Integer zip_code, Status status){
+        this.address = address;
+        this.zip_code = zip_code;
+        this.status = status;
+    }
 }
